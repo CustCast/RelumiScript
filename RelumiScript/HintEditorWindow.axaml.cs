47467519ebb16b5953e6ed33f0d80b1e79e8bbf7
CustCast <justinscottcuster@gmail.com>
2025-12-18 16:13:53 -0700

    draggable universal definition editor

--- RelumiScript/HintEditorWindow.axaml.cs
+++ RelumiScript/HintEditorWindow.axaml.cs
@@ -1,6 +1,7 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
+using Avalonia.Input;
 using Avalonia.Markup.Xaml;
 
 namespace RelumiScript
@@ -15,6 +16,14 @@ namespace RelumiScript
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
+        }
+
+        public void TitleBar_PointerPressed(object sender, PointerPressedEventArgs e)
+        {
+            if (e.GetCurrentPoint(this).Properties.IsLeftButtonPressed)
+            {
+                BeginMoveDrag(e);
+            }
         }
 
         public void OnSave_Click(object? sender, RoutedEventArgs e)
